a2fb70e3e2aed601cc5e3ea52088b9bd5b074827
NurtilekZ <nzakonbekov@gmail.com>
2026-04-13 08:01:22 +0600

    Add Asset and Settings files

--- Assets/TutorialInfo/Readme.cs
+++ Assets/TutorialInfo/Readme.cs
@@ -2,19 +2,22 @@
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class Readme : ScriptableObject
+namespace TutorialInfo
 {
-    public StyleSheet commonStyle;
-    public StyleSheet darkStyle;
-    public StyleSheet lightStyle;
-    public Texture2D icon;
-    public string title;
-    public Section[] sections;
-    public bool loadedLayout;
-
-    [Serializable]
-    public class Section
+    public class Readme : ScriptableObject
     {
-        public string heading, text, linkText, url;
+        public StyleSheet commonStyle;
+        public StyleSheet darkStyle;
+        public StyleSheet lightStyle;
+        public Texture2D icon;
+        public string title;
+        public Section[] sections;
+        public bool loadedLayout;
+
+        [Serializable]
+        public class Section
+        {
+            public string heading, text, linkText, url;
+        }
     }
 }
